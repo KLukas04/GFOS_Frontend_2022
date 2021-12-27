7b1d4921b37274433995fb744046b6f29483516b
Florian Noje <fl.noje@gmail.com>
2021-12-27 10:30:16 +0100

    Simple login form

--- src/app/authorization/authorization.component.ts
+++ src/app/authorization/authorization.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { faLock, faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-authorization',
   templateUrl: './authorization.component.html',
-  styleUrls: ['./authorization.component.scss']
+  styleUrls: ['./authorization.component.scss'],
 })
 export class AuthorizationComponent implements OnInit {
+  public faUser = faUser;
+  public faLock = faLock;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/authorization/authorization.module.ts
+++ src/app/authorization/authorization.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { AuthorizationRoutingModule } from './authorization-routing.module';
 import { AuthorizationComponent } from './authorization.component';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [
-    AuthorizationComponent
-  ],
-  imports: [
-    CommonModule,
-    AuthorizationRoutingModule
-  ]
+  declarations: [AuthorizationComponent],
+  imports: [CommonModule, AuthorizationRoutingModule, FontAwesomeModule],
 })
-export class AuthorizationModule { }
+export class AuthorizationModule {}
