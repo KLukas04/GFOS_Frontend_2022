0136a429344afbb8087fea0bbfe475453bbb2aa2
Florian Noje <fl.noje@gmail.com>
2022-01-05 15:59:06 +0100

    Simple category card

--- src/app/jobs/components/category-card/category-card.component.ts
+++ src/app/jobs/components/category-card/category-card.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-category-card',
   templateUrl: './category-card.component.html',
-  styleUrls: ['./category-card.component.scss']
+  styleUrls: ['./category-card.component.scss'],
 })
 export class CategoryCardComponent implements OnInit {
+  @Input() category: string = '';
+  @Input() jobs: number = 0;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
