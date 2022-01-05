93a325b8e23d5695eafcb3fd5cbe23ab83c937d1
Florian Noje <fl.noje@gmail.com>
2022-01-05 15:19:10 +0100

    Carousel does not end

--- src/app/jobs/components/trends/trends.component.ts
+++ src/app/jobs/components/trends/trends.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-trends',
   templateUrl: './trends.component.html',
-  styleUrls: ['./trends.component.scss']
+  styleUrls: ['./trends.component.scss'],
 })
 export class TrendsComponent implements OnInit {
+  public carouselIndex = 0;
+  public numberOfItems = 4;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  navigate(change: number): void {
+    this.carouselIndex = (this.carouselIndex + change) % this.numberOfItems;
+    console.log(this.carouselIndex);
   }
-
 }
