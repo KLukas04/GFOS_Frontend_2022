ad76b02af4900bace3fc454011b98e38aef080f1
Florian Noje <fl.noje@gmail.com>
2022-02-05 17:02:10 +0100

    Menu points added with hover animation

--- src/app/employer/employer.module.ts
+++ src/app/employer/employer.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { EmployerRoutingModule } from './employer-routing.module';
 import { EmployerComponent } from './employer.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { TuiLinkModule } from '@taiga-ui/core';
 
 @NgModule({
   declarations: [EmployerComponent, SidebarComponent],
-  imports: [CommonModule, EmployerRoutingModule],
+  imports: [CommonModule, EmployerRoutingModule, TuiLinkModule],
 })
 export class EmployerModule {}
