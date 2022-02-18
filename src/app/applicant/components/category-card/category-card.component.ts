8efc0c9c90b11ffb9653cf8e5a837e43822261dd
KLukas04 <krinkelukas@gmail.com>
2022-02-18 19:09:20 +0100

    added flexbox

--- src/app/applicant/components/category-card/category-card.component.ts
+++ src/app/applicant/components/category-card/category-card.component.ts
@@ -3,11 +3,10 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-category-card',
   templateUrl: './category-card.component.html',
-  styleUrls: ['./category-card.component.scss'],
+  styleUrls: ['./category-card.component.scss']
 })
 export class CategoryCardComponent implements OnInit {
   @Input() category: string = '';
-  @Input() jobs: number = 0;
 
   constructor() {}
 
