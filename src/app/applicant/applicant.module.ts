0e608ebe5d0c9662526ddfbf92b58b9c22bcf0c6
KLukas04 <krinkelukas@gmail.com>
2022-02-25 16:23:39 +0100

    changed font and color in card

--- src/app/applicant/applicant.module.ts
+++ src/app/applicant/applicant.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {TuiNotificationModule} from '@taiga-ui/core';
+import {
+  TuiNotificationModule,
+  TuiButtonModule
+} from '@taiga-ui/core';
 
 import {
   TuiBadgeModule,
   TuiIslandModule,
+  TuiAvatarModule
 } from '@taiga-ui/kit';
 
 import { ApplicantRoutingModule } from './applicant-routing.module';
@@ -12,6 +16,7 @@ import { ApplicantComponent } from './applicant.component';
 import { CategoryCardComponent } from './components/category-card/category-card.component';
 import { ApplicationCardComponent } from './components/application-card/application-card.component';
 import { InformationCardComponent } from './components/information-card/information-card.component';
+import { CvEditorComponent } from './components/cv-editor/cv-editor.component';
 
 
 @NgModule({
@@ -20,14 +25,16 @@ import { InformationCardComponent } from './components/information-card/informat
     CategoryCardComponent,
     ApplicationCardComponent,
     InformationCardComponent,
+    CvEditorComponent,
   ],
   imports: [
     CommonModule,
     ApplicantRoutingModule,
     TuiBadgeModule,
     TuiNotificationModule,
-    
+    TuiAvatarModule,
     TuiIslandModule,
+    TuiButtonModule
   ]
 })
 export class ApplicantModule { }
