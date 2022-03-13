15c03cc09d8bdb125a975f6427f658dc8bcafd0e
KLukas04 <krinkelukas@gmail.com>
2022-03-13 13:18:40 +0100

    application card bug fix

--- src/app/applicant/components/application-card/application-card.component.ts
+++ src/app/applicant/components/application-card/application-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { TuiStatusT } from '@taiga-ui/kit';
 
 @Component({
   selector: 'app-application-card',
@@ -7,6 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ApplicationCardComponent implements OnInit {
    @Input() title: string = '';
+   @Input() info: TuiStatusT = 'info';
    @Input() status: string = '';
   constructor() { }
 
