3fa0748666ed15e6df8b6f064673fb33e94bc191
KLukas04 <krinkelukas@gmail.com>
2022-04-18 17:14:12 +0200

    better ui

--- src/app/applicant/components/category-card/category-card.component.ts
+++ src/app/applicant/components/category-card/category-card.component.ts
@@ -8,7 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class CategoryCardComponent implements OnInit {
   @Input() category: string = '';
   @Input() link: string = '';
-  
+  @Input() img: string = '';
   constructor() {}
 
   ngOnInit(): void {}
