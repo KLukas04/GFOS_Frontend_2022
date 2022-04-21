4519705e244b937c074471ceabf4eed7d3866f8d
KLukas04 <krinkelukas@gmail.com>
2022-04-21 17:01:20 +0200

    ui for open applications

--- src/app/employer/components/myjobs/application-card/application-card.component.ts
+++ src/app/employer/components/myjobs/application-card/application-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-application-card',
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApplicationCardComponent implements OnInit {
 
+  @Input() name = "";
+  @Input() fachgebiet = "";
+  @Input() beworbenAm = "";
   constructor() { }
 
   ngOnInit(): void {
