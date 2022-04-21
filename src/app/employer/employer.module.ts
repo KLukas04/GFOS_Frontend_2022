c0c3f4e2954f602194f47d1899f184a5faa9a3dd
KLukas04 <krinkelukas@gmail.com>
2022-04-21 17:29:36 +0200

    tried implementing pdf view... doesnt work

--- src/app/employer/employer.module.ts
+++ src/app/employer/employer.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import {NgDompurifySanitizer} from '@tinkoff/ng-dompurify';
+import {PolymorpheusModule} from '@tinkoff/ng-polymorpheus';
+
 import { EmployerRoutingModule } from './employer-routing.module';
 import { EmployerComponent } from './employer.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { TuiButtonModule, TuiDialogModule, TuiLinkModule, TuiNotificationModule, TuiPrimitiveTextfieldModule, TuiRootModule, TuiScrollbarModule, TuiTextfieldControllerModule } from '@taiga-ui/core';
 import { TuiAxesModule, TuiLineChartModule } from '@taiga-ui/addon-charts';
-import { TuiAvatarModule, TuiBadgeModule, TuiCheckboxBlockModule, TuiInputModule, TuiIslandModule, TuiProgressModule, TuiTextAreaModule, TuiToggleModule } from '@taiga-ui/kit';
+import { TuiAvatarModule, TuiBadgeModule, TuiCheckboxBlockModule, TuiInputModule, TuiIslandModule, TuiPdfViewerModule, TuiProgressModule, TuiTextAreaModule, TuiToggleModule } from '@taiga-ui/kit';
 import { CreateJobComponent } from './components/createJob/create-job/create-job.component';
 import { HeaderComponent } from './components/createJob/header/header.component';
 import { ContentComponent } from './components/createJob/content/content.component';
@@ -16,6 +19,7 @@ import { MyJobsComponent } from './components/myjobs/my-jobs/my-jobs.component';
 import { JobOfferComponent } from './components/myjobs/job-offer/job-offer.component';
 import { ApplicationsForJobComponent } from './components/myjobs/applications-for-job/applications-for-job.component';
 import { ApplicationCardComponent } from './components/myjobs/application-card/application-card.component';
+import { TUI_SANITIZER } from '@taiga-ui/cdk';
 
 @NgModule({
   declarations: [EmployerComponent, SidebarComponent, CreateJobComponent, HeaderComponent, ContentComponent, MyJobsComponent, JobOfferComponent, ApplicationsForJobComponent, ApplicationCardComponent],
@@ -43,7 +47,15 @@ import { ApplicationCardComponent } from './components/myjobs/application-card/a
     TuiToggleModule,
     TuiScrollbarModule,
     TuiTextAreaModule,
-    TuiTextfieldControllerModule
+    TuiTextfieldControllerModule,
+    TuiPdfViewerModule,
+    PolymorpheusModule
   ],
+  providers:[
+    {
+      provide: TUI_SANITIZER,
+      useClass: NgDompurifySanitizer,
+    },
+  ]
 })
 export class EmployerModule {}
