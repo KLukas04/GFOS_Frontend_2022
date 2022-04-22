6a28e3a1e90b5f18778888ed5d461db69d9929ec
Florian Noje <fl.noje@gmail.com>
2022-04-22 17:45:57 +0200

    Trend job card correct icon

--- src/app/jobs/components/main/trend-job-card/trend-job-card.component.ts
+++ src/app/jobs/components/main/trend-job-card/trend-job-card.component.ts
@@ -13,4 +13,32 @@ export class TrendJobCardComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  searchIcon(id: number): string {
+    switch (id) {
+      case 0:
+        return 'assets/fachgebiete/coding.png';
+      case 1:
+        return 'assets/fachgebiete/cyber-security.png';
+      case 2:
+        return 'assets/fachgebiete/social-growth.png';
+      case 3:
+        return 'assets/fachgebiete/motivation.png';
+      case 4:
+        return 'assets/fachgebiete/budget.png';
+      case 5:
+        return 'assets/fachgebiete/support.png';
+      case 6:
+        return 'assets/fachgebiete/acquisation.png';
+      case 7:
+        return 'assets/fachgebiete/technician.png';
+      case 8:
+        return 'assets/fachgebiete/conveyor-belt.png';
+      case 9:
+        return 'assets/fachgebiete/shopping-cart.png';
+
+      default:
+        return '';
+    }
+  }
 }
