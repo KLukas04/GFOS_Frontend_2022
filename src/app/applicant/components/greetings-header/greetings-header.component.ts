fd719a7349ed8aba563e23218e2a2fa83c5bd26f
Florian Noje <fl.noje@gmail.com>
2022-04-29 13:15:55 +0200

    Name shown in greetings header

--- src/app/applicant/components/greetings-header/greetings-header.component.ts
+++ src/app/applicant/components/greetings-header/greetings-header.component.ts
@@ -1,5 +1,13 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { RemoteData } from 'ngx-remotedata';
+import { Observable } from 'rxjs';
+import { Account } from '../../models/account.model';
 
+import * as fromActions from '../../store/applicant.actions';
+import * as fromReducer from '../../store/applicant.reducer';
+import * as fromSelectors from '../../store/applicant.selectors';
 @Component({
   selector: 'app-greetings-header',
   templateUrl: './greetings-header.component.html',
@@ -8,7 +16,13 @@ import { Component, Input, OnInit } from '@angular/core';
 export class GreetingsHeaderComponent implements OnInit {
   @Input() amoutOfApplicaions: number = 0;
 
-  constructor() {}
+  public name$: Observable<RemoteData<Account, HttpErrorResponse>>;
 
-  ngOnInit(): void {}
+  constructor(private store: Store<fromReducer.ApplicantState>) {
+    this.name$ = this.store.select(fromSelectors.selectOwnAccount);
+  }
+
+  ngOnInit(): void {
+    this.store.dispatch(fromActions.loadOwnAccount());
+  }
 }
