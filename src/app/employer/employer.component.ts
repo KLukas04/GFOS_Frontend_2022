44a5ea1aad686cf5bbaa394b281ad34919f34a5b
Florian Noje <fl.noje@gmail.com>
2022-04-29 23:59:44 +0200

    First application shown on dashboard

--- src/app/employer/employer.component.ts
+++ src/app/employer/employer.component.ts
@@ -4,6 +4,7 @@ import { FormControl } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { RemoteData } from 'ngx-remotedata';
 import { Observable } from 'rxjs';
+import { Application } from './models/application.model';
 import { Employer } from './models/employer.model';
 import { Todo } from './models/todo.model';
 
@@ -19,17 +20,22 @@ import * as fromSelectors from './store/employer.selectors';
 export class EmployerComponent implements OnInit {
   public todos$: Observable<RemoteData<Todo[], HttpErrorResponse>>;
   public name$: Observable<RemoteData<Employer, HttpErrorResponse>>;
+  public applications$: Observable<
+    RemoteData<Application[], HttpErrorResponse>
+  >;
 
   public todoControl: FormControl = new FormControl(null);
 
   constructor(private store: Store<fromReducer.EmployerState>) {
     this.todos$ = this.store.select(fromSelectors.selectTodos);
     this.name$ = this.store.select(fromSelectors.selectOwnAccount);
+    this.applications$ = this.store.select(fromSelectors.selectApplications);
   }
 
   ngOnInit(): void {
     this.store.dispatch(fromActions.loadTodos());
     this.store.dispatch(fromActions.loadSelf());
+    this.store.dispatch(fromActions.loadApplications());
   }
 
   public saveTodo(): void {
