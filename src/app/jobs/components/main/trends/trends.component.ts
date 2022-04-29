62fb2845edd6dddbc8eb06a7cc72e55bcd60dd5f
KLukas04 <krinkelukas@gmail.com>
2022-04-29 13:53:41 +0200

    files

--- src/app/jobs/components/main/trends/trends.component.ts
+++ src/app/jobs/components/main/trends/trends.component.ts
@@ -4,6 +4,7 @@ import { Store } from '@ngrx/store';
 import { RemoteData } from 'ngx-remotedata';
 import { Observable } from 'rxjs';
 import { Job } from 'src/app/jobs/models/job.model';
+import { HostListener } from "@angular/core";
 
 import * as fromReducer from '../../../store/jobs.reducer';
 import * as fromSelectors from '../../../store/jobs.selectors';
@@ -17,11 +18,32 @@ export class TrendsComponent implements OnInit {
   public carouselIndex = 0;
   public numberOfItems = 1;
 
+  screenHeight: number = 0;
+  screenWidth: number = 0;
+
+  @HostListener('window:resize', ['$event'])
+    getScreenSize() {
+          this.screenHeight = window.innerHeight;
+          this.screenWidth = window.innerWidth;
+          if(this.screenWidth < 650){
+            this.numberOfItems = 1;
+          }else if(this.screenWidth < 1020){
+            this.numberOfItems = 2;
+          } else if(this.screenWidth < 1400){
+            this.numberOfItems = 3;
+          } else{
+            this.numberOfItems = 4;
+          }
+    }
+
   public trendJobs$: Observable<RemoteData<Job[], HttpErrorResponse>>;
 
   constructor(private store: Store<fromReducer.JobsState>) {
+    this.getScreenSize();
     this.trendJobs$ = this.store.select(fromSelectors.selectTrendJobs);
   }
+
+  
 
   ngOnInit(): void {}
 }
