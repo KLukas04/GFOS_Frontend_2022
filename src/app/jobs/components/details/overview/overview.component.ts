68b9a8fb169e618d124b40883da35d5cd11b71ca
Florian Noje <fl.noje@gmail.com>
2022-04-30 22:15:01 +0200

    Switch between chatt and apply

--- src/app/jobs/components/details/overview/overview.component.ts
+++ src/app/jobs/components/details/overview/overview.component.ts
@@ -1,15 +1,15 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { RemoteData } from 'ngx-remotedata';
-import { map, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Job } from 'src/app/jobs/models/job.model';
 
 import * as fromActions from '../../../store/jobs.actions';
 import * as fromReducer from '../../../store/jobs.reducer';
 import * as fromSelectors from '../../../store/jobs.selectors';
-import * as fromRouter from '../../../../store/router.selectors';
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
@@ -20,16 +20,19 @@ export class OverviewComponent implements OnInit {
   readonly control = new FormControl();
   private newFileBase64: any = '';
 
+  public showChat: boolean | undefined;
+
   public job$: Observable<RemoteData<Job, HttpErrorResponse>>;
 
-  public showChat$: Observable<boolean>;
-
-  constructor(private store: Store<fromReducer.JobsState>) {
+  constructor(
+    private store: Store<fromReducer.JobsState>,
+    private route: ActivatedRoute
+  ) {
     this.job$ = this.store.select(fromSelectors.selectSingleJob);
 
-    this.showChat$ = this.store
-      .select(fromRouter.selectQueryParams)
-      .pipe(map((params) => params['showChat']));
+    this.route.queryParams.subscribe((params) => {
+      this.showChat = params['showChat'] === 'true';
+    });
   }
 
   ngOnInit(): void {
